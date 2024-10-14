8a6f228dff3fecf70ebe5256b2eab4f4e84a0c74
chrisaut <chrisaut@github.com>
2024-10-14 19:48:15 +0200

    Fix build

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-@REM "%MSFS_SDK%Tools\bin\fspackagetool.exe" "chrisaut-toolbar-printouts\Build\chrisaut-toolbar-printouts.xml"
-copy /Y "chrisaut-toolbar-printouts\Build\Packages\chrisaut-toolbar-printouts\Build\chrisaut-toolbar-printouts.spb" "chrisaut-toolbar-printouts\InGamePanels"
+"%MSFS_SDK%Tools\bin\fspackagetool.exe" "chrisaut-toolbar-printouts\Build\chrisaut-toolbar-printouts.xml" -nopause
+xcopy /Y "chrisaut-toolbar-printouts\Build\Packages\chrisaut-toolbar-printouts\Build\chrisaut-toolbar-printouts.spb" "chrisaut-toolbar-printouts\InGamePanels"
 xcopy /Y /I "chrisaut-toolbar-printouts\manifest.json" "release\chrisaut-toolbar-printouts\"
 xcopy /Y /I "chrisaut-toolbar-printouts\Build\Packages\chrisaut-toolbar-printouts\Build\chrisaut-toolbar-printouts.spb" "release\chrisaut-toolbar-printouts\InGamePanels\"
 xcopy /E /Y /I "chrisaut-toolbar-printouts\html_ui\*" "release\chrisaut-toolbar-printouts\html_ui\"
